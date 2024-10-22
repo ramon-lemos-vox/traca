0b2ce82366683d10730c8d838ec9a54c5b377ca8
Ramon <johnnyramon2011@hotmail.com>
2024-10-22 17:50:03 -0300

    Initial

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Função para buscar os usuários
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://api-traca.voxcity.com.br');
+        console.log('Users:', response.data);
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers(); 
+  }, []); 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Users:</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.username} - {user.email}
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
