ab41e76f4891b6d500218e973e7701f3e98dc070
Ramon <johnnyramon2011@hotmail.com>
2024-10-22 18:09:35 -0300

    api

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     // Função para buscar os usuários
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('http://186.237.57.35');
+        const response = await axios.get('http://api-traca.voxcity.com.br');
         console.log('Users:', response.data);
         setUsers(response.data);
       } catch (error) {
