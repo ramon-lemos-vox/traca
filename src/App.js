d90b8caa90ed013189b8dd06925f7ee80acccae1
Ramon <johnnyramon2011@hotmail.com>
2024-10-24 10:53:54 -0300

    pronto para o will trabalhar

--- src/App.js
+++ src/App.js
@@ -5,36 +5,25 @@ import './App.css';
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    // Função para buscar os usuários
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get('https://api-traca.voxcity.com.br');
-        console.log('Users:', response.data);
-        setUsers(response.data);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
+  // useEffect(() => {
+  //   // Função para buscar os usuários
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const response = await axios.get('https://api-traca.voxcity.com.br');
+  //       console.log('Users:', response.data);
+  //       setUsers(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching users:', error);
+  //     }
+  //   };
 
-    fetchUsers(); 
-  }, []); 
+  //   fetchUsers(); 
+  // }, []); 
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Deploy de novo
-          <h1>vox</h1>
-          
-        </h1>
-        <h2>Users:</h2>
-        <ul>
-          {users?.map((user) => (
-            <li key={user.id}>
-              {user.username} - {user.email}
-            </li>
-          ))}
-        </ul>
+        <h2>Bem vindo Will</h2>
       </header>
     </div>
   );
